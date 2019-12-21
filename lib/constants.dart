350cb7f2e0940b9029772fffc782c3dea864635b
Anwar Abir <abuanwar072@gmail.com>
2019-12-21 13:13:59 +0600

    constant file update

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kButtonColor = Color(0xFFe76158);
+const kBackground = Color(0xFF1d1c22);
+const kTextColor = Color(0xFFe4e3e6);
+
+Widget logo = Image.asset('assets/images/netflix_logo.png');
